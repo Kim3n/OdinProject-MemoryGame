ffaf3be54d66a6f7457ae60bd8227337810a20dd
Kim <lademokim@hotmail.com>
2024-11-11 12:30:03 +0100

    Changed link to point to repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa";
 function Footer() {
     return (
         <footer>
-            <a href="https://github.com/Kim3n"><FaGithub /><h2>Made by Kim3n</h2></a>
+            <a href="https://github.com/Kim3n/OdinProject-MemoryGame"><FaGithub /><h2>Made by Kim3n</h2></a>
         </footer>
     )
 }
